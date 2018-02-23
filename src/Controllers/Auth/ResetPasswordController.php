ad57f1fcf572fab2e02e2fd78f6fdcb19a049780
hostato <info@hostato.com>
2018-02-23 12:16:57 +0500

    publishig resources and translations

--- src/Controllers/Auth/ResetPasswordController.php
+++ src/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace Adam\EmailConfirm\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
